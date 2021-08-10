c94bbd05c4a25143faf3ae916a450151362dcd62
Sanha <47966951+sweetcorn1229@users.noreply.github.com>
2021-08-10 15:01:41 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -57,13 +57,50 @@ function sep (letter, option) {
   ];
 }
 
+function sep (letter, option) {
+  let k = letter.charCodeAt() - 44032;
+  let cho, jung, jong;
+  for (let i = 1; i <= 19; i++) {
+    if (k < i*21*28) {
+      k -= (i-1)*21*28;
+      for (let j = 1; j <= 21; j++) {
+        if (k < j*28) {
+          k -= (j-1)*28;
+          cho = i-1, jung = j-1, jong = k;
+          break;
+        }
+      }
+      break;
+    }
+  }
+  return [
+    CHOSUNG[cho], JUNGSUNG[jung], JONGSUNG[jong]
+  ];
+}
+
 function con (frag, option) {
   let icho = CHOSUNG.indexOf(frag[0]);
   let ijung = JUNGSUNG.indexOf(frag[1]);
-  let ijong = JONGSUNG.indexOf(frag[2]);
+  let ijong = frag[2] ? JONGSUNG.indexOf(frag[2]) : 0;
   
   let cc = 44032+icho*21*28+ijung*28+ijong;
   return String.fromCharCode(cc);
+}
+
+function cfa (fragArray, option) {
+  return fragArray.map(el => con(el)).join('');
+}
+
+function cfs (fragString, option) {
+  let b = fragString.split('').map(el => Number(JUNGSUNG.includes(el))).join('').replace(/00/g, '0||0').split('||').map(el => el.length > 3 ? el.replace(/10/g, '1||0') : el).join('||').split('||').map(el => el.length);
+  let result = String();
+  let fsc = String(fragString);
+  for (let v of b) {
+    Log.d(fsc.slice(0, v));
+    result += con(fsc.slice(0, v).split(''));
+    fsc = fsc.slice(v);
+  }
+  return result;
 }
 
 module.exports = {
