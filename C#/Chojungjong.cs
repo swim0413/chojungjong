106106da02f6ff451871d065557f45722855bc2e
swim <69084389+swim0413@users.noreply.github.com>
2022-08-07 22:08:09 +0900

    C#
    C#

---
+++ C#/Chojungjong.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chojungjong
+{
+	public static class cjj
+	{
+
+		public static string[] CHOSUNG = new string[] {
+				"ㄱ", "ㄲ", "ㄴ",
+				"ㄷ", "ㄸ", "ㄹ",
+				"ㅁ", "ㅂ", "ㅃ",
+				"ㅅ", "ㅆ", "ㅇ",
+				"ㅈ", "ㅉ", "ㅊ",
+				"ㅋ", "ㅌ", "ㅍ",
+				"ㅎ"
+			};
+
+		public static string[] JUNGSUNG = new string[] {
+				"ㅏ", "ㅐ", "ㅑ",
+				"ㅒ", "ㅓ", "ㅔ",
+				"ㅕ", "ㅖ", "ㅗ",
+				"ㅘ", "ㅙ", "ㅚ",
+				"ㅛ", "ㅜ", "ㅝ",
+				"ㅞ", "ㅟ", "ㅠ",
+				"ㅡ", "ㅢ", "ㅣ"
+			};
+
+		public static string[] JONGSUNG = new string[]{
+				" ", "ㄱ", "ㄲ",
+				"ㄳ", "ㄴ", "ㄵ",
+				"ㄶ", "ㄷ", "ㄹ",
+				"ㄺ", "ㄻ", "ㄼ",
+				"ㄽ", "ㄾ", "ㄿ",
+				"ㅀ", "ㅁ", "ㅂ",
+				"ㅄ", "ㅅ", "ㅆ",
+				"ㅇ", "ㅈ", "ㅊ",
+				"ㅋ", "ㅌ", "ㅍ",
+				"ㅎ"
+			};
+
+		public static string[] separate(string letter)
+		{
+			int k = (int)letter[0] - 44032;
+			int cho = 0, jung = 0, jong = 0;
+			for (int i = 1; i <= 19; i++)
+			{
+				if (k < i * 21 * 28)
+				{
+					k -= (i - 1) * 21 * 28;
+					for (int j = 1; j <= 21; j++)
+					{
+						if (k < j * 28)
+						{
+							k -= (j - 1) * 28;
+							cho = i - 1;
+							jung = j - 1;
+							jong = k;
+							break;
+						}
+					}
+					break;
+				}
+			}
+			return new string[] { CHOSUNG[cho], JUNGSUNG[jung], JONGSUNG[jong] };
+		}
+
+		public static List<string[]> sfa(string word)
+		{
+			List<string[]> res = new List<string[]> { };
+			for (int i = 0; i < word.Split(" ").Length; i++)
+			{
+				res.Add(separate(word.Split(" ")[i]));
+			}
+			return res;
+		}
+
+		public static string concat(string[] frags)
+		{
+			int icho = Array.IndexOf(CHOSUNG, frags[0]);
+			int ijung = Array.IndexOf(JUNGSUNG, frags[1]);
+			int ijong = 0;
+			if (frags.Length >= 3)
+			{
+				ijong = Array.IndexOf(JONGSUNG, frags[2]);
+			}
+			else
+			{
+				ijong = 0;
+			}
+			int cc = 44032 + icho * 21 * 28 + ijung * 28 + ijong;
+			return Convert.ToString((char)cc);
+		}
+
+		public static string cfa(List<string[]> fragArray)
+		{
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < fragArray.Count; i++)
+			{
+				sb.Append(concat(fragArray[i]));
+			}
+			return sb.ToString();
+		}
+	}
+}
+
---
+++ C#/Program.cs
@@ -0,0 +1,20 @@
+﻿using Chojungjong;
+
+//concat => 한음절
+Console.WriteLine(cjj.concat(new string[] { "ㅅ", "ㅐ", "ㄴ" }));
+
+//cfa => 여러 음절
+Console.WriteLine(cjj.cfa(new List<string[]> { new string[] { "ㅅ", "ㅜ" }, new string[] { "ㅇ", "ㅕ", "ㅇ" }, new string[] { "ㅇ", "ㅏ" } }));
+
+//separate => 한음절
+cjj.separate("쀍").ToList().ForEach(Console.WriteLine);
+
+//sfa => 여러 음절
+String word = "안 녕"; //음절 단위 뛰어적기
+for(int i = 0; i < word.Split(" ").Length; i++)
+{
+    for(int j =0; j < cjj.separate(word.Split(" ")[i]).Length; j++)
+    {
+        Console.WriteLine(cjj.sfa(word)[i][j]);
+    }
+}
